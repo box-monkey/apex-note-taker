518314e5219d908fc84556936ba8baac047dce22
box-monkey <jr.gasparin15@gmail.com>
2022-01-24 10:57:34 -0500

    added note creation functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,15 @@
-const path = require('path');
-const router = require('express').Router();
+const router = require("express").Router();
+const { createNewNote } = require("../lib/notes");
 
-// * will reroute any other input to this page
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+// requests and gets data from our notes file.
+router.get("/notes", (req, res) => {
+  console.log("route hit");
+  const incomingGetNotes = getNotes();
+  res.json(incomingGetNotes);
 });
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-})
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-module.exports = router; 
+module.exports = router;
